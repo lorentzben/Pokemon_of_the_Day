0428298d5d056ff3c4eb2ed005082140ba132780
Ben Lorentz <blorentz@luc.edu>
2020-10-03 13:42:55 -0400

    added js

---
+++ index.js
@@ -0,0 +1,10 @@
+let pokeDiv = document.getElementById('pokemon')
+
+var poke = Math.floor(Math.random()* 1050) + 1
+var url = "https://pokeapi.co/api/v2/pokemon/"
+var req = url.concat(poke,"/")
+fetch(req)
+.then(res => res.json())
+.then(pokemon => {
+    pokeDiv.innerHTML += '<p> ${pokemon.name} </p>'
+})
