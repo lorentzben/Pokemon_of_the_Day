b0e1b0b5efb4db8bc84bf224ddf1883e3a910456
Ben Lorentz <blorentz@luc.edu>
2020-10-03 14:19:01 -0400

    it works, the button does nothing tho

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
+
+
 let pokeDiv = document.getElementById('pokemon')
 
 var poke = Math.floor(Math.random()* 1050) + 1
 var url = "https://pokeapi.co/api/v2/pokemon/"
 var req = url.concat(poke,"/")
+
+console.log(req)
+
 fetch(req)
 .then(res => res.json())
 .then(pokemon => {
-    pokeDiv.innerHTML += '<p> ${pokemon.name} </p>'
+    pokeDiv.innerHTML += `<p> ${pokemon.name} </p>`
 })
